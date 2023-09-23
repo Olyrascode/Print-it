edddcffb1ff797e2481087cf969c77ece585db61
Lucas <kingbanxit@gmail.com>
2023-09-24 00:15:44 +0200

    fleches final

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,58 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
-const arrowLeft = document.getElementById('arrow_left');
-const arrowRight = document.getElementById('arrow_right');
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
+// recuperation image fleche//
+let leftArrow = document.createElement("img");
+let rightArrow = document.createElement("img");
 
-function clicArrowLeft() {
-	console.log('click fleche gauche')
-};
-function clicArrowRight() {
-	console.log('click fleche droite')
-};
-arrowLeft.addEventListener('click', clicArrowLeft);
-arrowRight.addEventListener('click', clicArrowRight);
+leftArrow.src = "assets/images/arrow_left.png";
+leftArrow.alt = "arrow left";
+leftArrow.id = "arrow_left";
+leftArrow.className = 'arrow';
+
+
+rightArrow.src = "assets/images/arrow_right.png";
+rightArrow.alt = "arrow right";
+rightArrow.id = 'arrow_right';
+rightArrow.className = 'arrow';
+
+// Sélectionnez l'élément "arrowDiv" par sa classe
+let arrowDiv = document.querySelector('#banner');
+
+
+
+
+
+// attribution //
+arrowDiv.insertBefore(rightArrow, arrowDiv.querySelector('p'));
+arrowDiv.insertBefore(leftArrow, arrowDiv.querySelector('p'));
+
+
+
+
+// listeneer fleches //
+leftArrow.addEventListener("click", function() {
+  alert("Image gauche cliquée !");
+});
+
+rightArrow.addEventListener("click", function() {
+  alert("Image droite cliquée !");
+});
+//bullet point//
+
